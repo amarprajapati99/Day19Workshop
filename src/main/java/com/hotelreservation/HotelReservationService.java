d72a586b81ccb123d0fcdac07532f6b6e5577e22
amarprajapati99 <amarprajapati99@gmail.com>
2021-04-26 07:00:26 +0530

    To find the cheapest hotel to given range

--- src/main/java/com/hotelreservation/HotelReservationService.java
+++ src/main/java/com/hotelreservation/HotelReservationService.java
@@ -1,10 +1,15 @@
 package com.hotelreservation;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 /* @Description - Add hotel in hotel reservation system */
 
 public class HotelReservationService {
+    private static final DateTimeFormatter DATE_RANGE_FORMAT = DateTimeFormatter.ofPattern("ddMMMyyyy");
     private List<Hotel> hotels;
 
     public HotelReservationService() {
@@ -12,5 +17,22 @@ public class HotelReservationService {
     }
     public boolean addHotel(Hotel hotel) {
         return this.hotels.add(hotel);
+    }
+    /* @Description - To find the cheapest hotel to given range.*/
+
+    public List<Result> findCheapestHotelsBasedOnDay(CustomerType customerType, String initialDateRange, String endDateRange) {
+        LocalDate initialDate = LocalDate.parse(initialDateRange, DATE_RANGE_FORMAT);
+        LocalDate endDate = LocalDate.parse(endDateRange, DATE_RANGE_FORMAT);
+
+        List<Result> results = this.hotels.stream()
+                .map(hotel -> {
+                    Result result = new Result();
+                    result.setHotelName(hotel.name);
+                    result.setTotalRate(hotel.getTotalRate(customerType, initialDate, endDate));
+                    return result;
+                }).sorted(Comparator.comparing(Result::getTotalRate)).collect(Collectors.toList());
+
+        return results.stream().filter(result -> result.getTotalRate()==results.get(0).getTotalRate())
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/om/hotelreservation/HotelReservationServiceTest.java
+++ src/test/java/om/hotelreservation/HotelReservationServiceTest.java
@@ -1,24 +1,55 @@
 package om.hotelreservation;
 
-import com.hotelreservation.Hotel;
-import com.hotelreservation.HotelReservationService;
+import com.hotelreservation.*;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class HotelReservationServiceTest {
     private HotelReservationService hotelReservationService;
     private Hotel lakewood;
+    private Hotel bridgewood;
+    private Hotel ridgewood;
 
     @Before
     public void setup() throws Exception {
         hotelReservationService = new HotelReservationService();
-        lakewood = new Hotel("Lakewood", 4);
+        HashMap<CustomerType, Rate> customerTypeRateMap = new HashMap<>();
+        customerTypeRateMap.put(CustomerType.regular,new Rate(110,90));
+        customerTypeRateMap.put(CustomerType.reward, new Rate(80,80));
+        lakewood = new Hotel("Lakewood",3, customerTypeRateMap);
+
+        customerTypeRateMap = new HashMap<>();
+        customerTypeRateMap.put(CustomerType.regular,new Rate(160,40));
+        customerTypeRateMap.put(CustomerType.reward, new Rate(110,50));
+        bridgewood = new Hotel("Bridgewood", 4, customerTypeRateMap);
+
+        customerTypeRateMap = new HashMap<>();
+        customerTypeRateMap.put(CustomerType.regular,new Rate(220,150));
+        customerTypeRateMap.put(CustomerType.reward, new Rate(100,40));
+        ridgewood = new Hotel("Ridgewood", 5, customerTypeRateMap);
+        hotelReservationService.addHotel(lakewood);
+        hotelReservationService.addHotel(bridgewood);
+        hotelReservationService.addHotel(ridgewood);
     }
 
     @Test
     public void givenHotel_whenInvokedAddHotel_shouldBeAbleToAdd() {
-        Assertions.assertTrue(this.hotelReservationService.addHotel(lakewood));
+        Assert.assertTrue(this.hotelReservationService.addHotel(lakewood));
+    }
+
+    @Test
+    public void givenDateRange_whenSearched_shouldReturnCheapestHotelBasedOnWeekdayAndWeekend() {
+        List<Result> cheapestHotelResult =
+                hotelReservationService.findCheapestHotelsBasedOnDay(CustomerType.regular,
+                        "11Sep2020", "12Sep2020");
+
+        Assertions.assertEquals(2,cheapestHotelResult.size());
+        Assertions.assertEquals(200,cheapestHotelResult.get(0).getTotalRate());
     }
 
 }
